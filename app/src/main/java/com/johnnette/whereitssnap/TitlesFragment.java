bf048438442a8971c5b44591343002e17e958f53
himanshu <himanshu.johnnette@gmail.com>
2024-01-15 17:46:13 +0530

    Chapter 21 [527] navDrawerLayout

---
+++ app/src/main/java/com/johnnette/whereitssnap/TitlesFragment.java
@@ -0,0 +1,14 @@
+package com.johnnette.whereitssnap;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+public class TitlesFragment extends ListFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
